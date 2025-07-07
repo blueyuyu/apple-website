914c9254a0b5c3706845711c10d8b762987f55b1
chenyu <m8e-h9hsjrhej@dingtalk.com>
2025-07-07 18:13:23 +0800

    引入动画

--- src/components/VideoScroll.tsx
+++ src/components/VideoScroll.tsx
@@ -1,29 +1,67 @@
 import { hightlightsSlides } from "@/constant/index.ts";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef, useState } from "react";
 
 export const VideoScroll = () => {
+  const [video, setVideo] = useState({
+    isEnd: false,
+    isStart: false,
+    isPlaying: false,
+    isPaused: false,
+    videoId: 0,
+  });
+
+  const { isEnd, isStart, isPlaying, isPaused, videoId } = video;
+  useGSAP(() => {
+    gsap.to("#slider", {
+      transform: `translateX(${videoId * -100}%)`,
+      duration: 2,
+      ease: "power2.inOut",
+    });
+  }, [videoId]);
+
+  // setInterval(() => {
+  //   if (videoId < 4) {
+  //     console.log("end", videoId);
+  //     setVideo({
+  //       ...video,
+  //       videoId: videoId + 1,
+  //     });
+  //   } else {
+  //     setVideo({
+  //       ...video,
+  //       videoId: 0,
+  //     });
+  //   }
+  // }, 2000);
+
   return (
     <div>
       <div className="flex items-center">
         {hightlightsSlides.map((item, index) => (
-          <div className="video-container">
-            <div className="relative video-box h-auto" key={index}>
-              <video
-                autoPlay
-                muted
-                width="100%"
-                height="100%"
-                className="hight-video h-auto object-cover"
-              >
-                <source src={item.video} type="video/mp4" />
-              </video>
-            </div>
-            <div className="box-desc">
-              <p>1112222</p>
+          <div id="slider" key={item.id}>
+            <div className="video-container relative">
+              <div className="w-full h-full">
+                <video autoPlay muted className="hight-video" width="100%">
+                  <source src={item.video} width="100%" type="video/mp4" />
+                </video>
+              </div>
+              <div className="box-desc">
+                {item.textLists.map((textItem, textIndex) => (
+                  <p
+                    key={textIndex}
+                    className="md:text-2xl text-xl font-medium text-white"
+                  >
+                    {textItem}
+                  </p>
+                ))}
+              </div>
             </div>
           </div>
         ))}
       </div>
-      <div className="video-dots">视频容器的点点</div>
+      <div className="video-dots text-white">视频容器的点点2222</div>
     </div>
   );
 };
--- src/components/highlight.tsx
+++ src/components/highlight.tsx
@@ -3,17 +3,17 @@ import { VideoScroll } from "./VideoScroll.tsx";
 
 export const Highlight = () => {
   return (
-    <div className="w-full h-screen px-30 py-20">
+    <div className="w-full h-screen px-30 py-20 bg-[#101010]">
       <div className="highlight-title w-[80%] mb-12 pl-4 sm:pl-0 md:mx-auto lg:flex lg:justify-between lg:items-center">
         <h1 className="section-title pb-4 lg:pb-0">Get the highlights.</h1>
         <div className="title-link md:flex gap-4">
           <div className="section-link flex gap-2 items-center pb-4 md:pb-0">
             Watch the film
-            <img src={watch} alt="watch" srcset="" />
+            <img src={watch} alt="watch" />
           </div>
           <div className="section-link flex gap-2 items-center">
             Watch the event
-            <img src={right} alt="right" srcset="" />
+            <img src={right} alt="right" />
           </div>
         </div>
       </div>
